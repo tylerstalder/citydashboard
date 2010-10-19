db026b8bca4526cc473c55ab21ea3a7823f2c58c
Dan Melton <melton.dan@gmail.com>
2010-10-19 16:55:06 -0700

    Added a if statement to check for table existence in initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Setting.twitter_key.first.setting, Setting.twitter_secret.first.setting
-  provider :facebook, Setting.facebook_key.first.setting, Setting.facebook_secret.first.setting  
-  provider :github, Setting.github_key.first.setting, Setting.github_secret.first.setting    
+  if ActiveRecord::Base.connection.tables.include?("settings")
+    provider :twitter, Setting.twitter_key.first.setting, Setting.twitter_secret.first.setting
+    provider :facebook, Setting.facebook_key.first.setting, Setting.facebook_secret.first.setting  
+    provider :github, Setting.github_key.first.setting, Setting.github_secret.first.setting   
+  end 
 end
